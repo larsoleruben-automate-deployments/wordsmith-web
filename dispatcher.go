60089d908c41e349bb1f8fb3a4c6fa250a6eef10
Lars Ole Christoffersen <2634802+larsoleruben@users.noreply.github.com>
2023-07-11 16:07:43 +0200

    Fidling with go

--- dispatcher.go
+++ dispatcher.go
@@ -1,73 +1,81 @@
-	package main
+package main
 
-	import (
-		"fmt"
-		"io/ioutil"
-		"log"
-		"math/rand"
-		"net"
-		"net/http"
-		"time"
-	)
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
+	"time"
+)
 
-	func main() {
-		// Seed the random number generator
-		rand.Seed(time.Now().UnixNano())
+func main() {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
 
-		fwd := &forwarder{"api", 8080}
-		http.Handle("/words/", http.StripPrefix("/words", fwd))
-		http.Handle("/", http.FileServer(http.Dir("static")))
+	fwd := &forwarder{"api", 8080}
+	http.Handle("/words/", http.StripPrefix("/words", fwd))
+	http.Handle("/", http.FileServer(http.Dir("static")))
 
-		fmt.Println("Listening on port 80")
-		log.Fatal( http.ListenAndServe(":80", nil))
+	s := &http.Server{
+		Addr:           ":80",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Minute,
+		WriteTimeout:   10 * time.Minute,
+		MaxHeaderBytes: 0,
+	}
+	http_error := s.ListenAndServe()
+	if http_error != nil {
+		log.Println("Error", http_error)
+	}
+}
 
+type forwarder struct {
+	host string
+	port int
+}
+
+func (f *forwarder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	addrs, err := net.LookupHost(f.host)
+	if err != nil {
+		log.Println("Error", err)
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
-	type forwarder struct {
-		host string
-		port int
+	log.Printf("%s %d available ips: %v", r.URL.Path, len(addrs), addrs)
+	ip := addrs[rand.Intn(len(addrs))]
+	log.Printf("%s I choose %s", r.URL.Path, ip)
+
+	url := fmt.Sprintf("http://%s:%d%s", ip, f.port, r.URL.Path)
+	log.Printf("%s Calling %s", r.URL.Path, url)
+
+	if err = copy(url, ip, w); err != nil {
+		log.Println("Error", err)
+		http.Error(w, err.Error(), 500)
+		return
 	}
+}
 
-	func (f *forwarder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-		addrs, err := net.LookupHost(f.host)
-		if err != nil {
-			log.Println("Error", err)
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		log.Printf("%s %d available ips: %v", r.URL.Path, len(addrs), addrs)
-		ip := addrs[rand.Intn(len(addrs))]
-		log.Printf("%s I choose %s", r.URL.Path, ip)
-
-		url := fmt.Sprintf("http://%s:%d%s", ip, f.port, r.URL.Path)
-		log.Printf("%s Calling %s", r.URL.Path, url)
-
-		if err = copy(url, ip, w); err != nil {
-			log.Println("Error", err)
-			http.Error(w, err.Error(), 500)
-			return
-		}
-	}
-
-	func copy(url, ip string, w http.ResponseWriter) error {
-		resp, err := http.Get(url)
-		if err != nil {
-			return err
-		}
-
-		for header, values := range resp.Header {
-			for _, value := range values {
-				w.Header().Add(header, value)
-			}
-		}
-		w.Header().Set("source", ip)
-
-		buf, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-
-		_, err = w.Write(buf)
+func copy(url, ip string, w http.ResponseWriter) error {
+	resp, err := http.Get(url)
+	if err != nil {
 		return err
 	}
+
+	for header, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(header, value)
+		}
+	}
+	w.Header().Set("source", ip)
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(buf)
+	return err
+}
